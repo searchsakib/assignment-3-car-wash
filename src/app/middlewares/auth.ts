e973e8b84e5fdb0e7136a05eedcfc0c9db665dbc
searchsakib <searchsakib@gmail.com>
2024-06-13 02:00:52 +0600

    user module created, auth middleware edited

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -3,13 +3,13 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import AppError from '../errors/AppError';
-import { TUserRole } from '../modules/user/user.interface';
-import { User } from '../modules/user/user.model';
+import { TUserRoles } from '../modules/user/user.interface';
 import catchAsync from '../utils/catchAsync';
+import { User } from '../modules/user/user.model';
 
-const auth = (...requiredRoles: TUserRole[]) => {
+const auth = (...requiredRoles: TUserRoles[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1];
     // checking if the token is missing
     if (!token) {
       throw new AppError(
@@ -20,34 +20,34 @@ const auth = (...requiredRoles: TUserRole[]) => {
 
     // checking if the given token is valid
     const decoded = jwt.verify(
-      token,
+      token as string,
       config.jwt_access_secret as string
     ) as JwtPayload;
 
-    const { role, userId, iat } = decoded;
+    // extracting email & role from decoded
+    const { email, role } = decoded;
+
+    const user = User.findOne({ email, role });
 
     // checking if the user is exist
-    const user = await User.isUserExistsByCustomId(userId);
-
     if (!user) {
-      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
-    }
-    // checking if the user is already deleted
-
-    const isDeleted = user?.isDeleted;
-
-    if (isDeleted) {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
-    }
-
-    if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
         'You have no access to this route'
       );
     }
 
-    req.user = decoded as JwtPayload;
+    if (!requiredRoles.includes(role)) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        'You have no access to this route'
+      );
+    }
+    req.user = {
+      email,
+      role,
+    };
+
     next();
   });
 };
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,54 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+import config from '../../config';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+export const User = model<TUser>('User', userSchema);
