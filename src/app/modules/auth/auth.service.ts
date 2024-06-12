719f810636318f428fdc1267100ae403248d4b1c
searchsakib <searchsakib@gmail.com>
2024-06-13 02:35:54 +0600

    auth interface,service,validation created

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import AppError from '../../errors/AppError';
+import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
+import { TUser } from '../user/user.interface';
+import { User } from '../user/user.model';
+import httpStatus from 'http-status';
+
+const signUp = async (payload: TUser) => {
+  const result = await User.create(payload);
+  return result;
+};
+
+const login = async (payload: TLoginUser) => {
+  const user = await User.findOne({ email: payload?.email }).select(
+    '+password'
+  );
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User does not exists');
+  }
+
+  const match = await bcrypt.compare(payload?.password, user?.password);
+  if (!match) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Password is incorrect');
+  }
+  const jwtPayload = {
+    email: user?.email,
+    role: user?.role,
+  };
+
+  const token = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: config.jwt_access_expires_in,
+  });
+
+  return {
+    token,
+    user,
+  };
+};
+
+export const AuthServices = {
+  signUp,
+  login,
+};
---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const loginValidationSchema = z.object({
+  body: z.object({
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email({ message: 'Invalid email' }),
+    password: z.string({ required_error: 'password is required' }),
+  }),
+});
+
+export const AuthValidations = {
+  loginValidationSchema,
+};
